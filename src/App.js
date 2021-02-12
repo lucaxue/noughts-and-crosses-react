db0611f0f63c5396eb6ee4229cbb4b8716af9364
lucaxue <lxue02@gmail.com>
2021-02-12 09:53:27 +0000

    converted board to functional comp

--- src/App.js
+++ src/App.js
@@ -1,34 +1,55 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
 
 //component - square is a button
 function Square() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+  return <button className="square">{/* TODO */}</button>;
+}
+
+const boardRow1 = [0, 1, 2];
+const boardRow2 = [3, 4, 5];
+const boardRow3 = [6, 7, 8];
 
 function Board() {
-  
-
+  const [status, setStatus] = useState('Next player: X');
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {/* three sq keys 0 1 2  */}
+        {boardRow1.map((SqNum) => (
+          <Square key={SqNum} />
+        ))}
+      </div>
+      <div className="board-row">
+        {/* three sq keys 3 4 5 */}
+        {boardRow2.map((SqNum) => (
+          <Square key={SqNum} />
+        ))}
+      </div>
+      <div className="board-row">
+        {/* three sq keys 6 7 8 */}
+        {boardRow3.map((SqNum) => (
+          <Square key={SqNum} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 function Game() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
       </div>
-    );
-  }
-
+      <div className="game-info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
+  );
+}
 
 export default Game;
