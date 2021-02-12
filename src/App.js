a9816a7367ddd590dc6eab50b6d1b988a60ad7dd
lucaxue <lxue02@gmail.com>
2021-02-12 10:22:29 +0000

    squares state

--- src/App.js
+++ src/App.js
@@ -3,35 +3,39 @@ import './App.css';
 import React, { useState } from 'react';
 
 //component - square is a button
-function Square({value}) {
-  return <button className="square" onClick={() => alert('you have clicked me')}>{value}</button>;
+function Square(props) {
+  //usestate of value
+  const [value, setValue] = useState(null);
+  return (
+    <button className="square" onClick={() => setValue('X')}>
+      {value}
+    </button>
+  );
 }
-
-const boardRow1 = [0, 1, 2];
-const boardRow2 = [3, 4, 5];
-const boardRow3 = [6, 7, 8];
 
 function Board() {
   const [status, setStatus] = useState('Next player: X');
+  //state of squares
+  const [squares, setSquares] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+
   return (
     <div>
       <div className="status">{status}</div>
       <div className="board-row">
         {/* three sq keys 0 1 2  */}
-        {boardRow1.map((SqNum) => (
-          <Square key={SqNum} value={SqNum} />
+        {squares.slice(0, 3).map((item) => (
+          <Square key={item} value={item} />
         ))}
       </div>
       <div className="board-row">
         {/* three sq keys 3 4 5 */}
-        {boardRow2.map((SqNum) => (
-          <Square key={SqNum} value={SqNum} />
+        {squares.slice(3, 6).map((item) => (
+          <Square key={item} value={item} />
         ))}
       </div>
       <div className="board-row">
-        {/* three sq keys 6 7 8 */}
-        {boardRow3.map((SqNum) => (
-          <Square key={SqNum} value={SqNum} />
+        {squares.slice(6).map((item) => (
+          <Square key={item} value={item} />
         ))}
       </div>
     </div>
